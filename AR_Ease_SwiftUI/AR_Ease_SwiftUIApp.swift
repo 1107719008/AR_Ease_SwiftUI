82cd4a0f5696fb796d62ca8cca06b1a6386adb88
Chun-Han-Lin <hanklin0208@gmail.com>
2023-06-09 14:53:54 +0800

    Initial Commit

---
+++ AR_Ease_SwiftUI/AR_Ease_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_Ease_SwiftUIApp.swift
+//  AR_Ease_SwiftUI
+//
+//  Created by 林君翰 on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct AR_Ease_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR_Ease_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AR_Ease_SwiftUI
+//
+//  Created by 林君翰 on 2023/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
